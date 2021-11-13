a1bc06398b2986d5b1252edda611bdeb16a99edd
Samuel VIDEAU <samuelvideau@yahoo.fr>
2021-11-13 16:34:01 +0100

    chore: kovan env

--- src/environments/environment.mumbai.ts
+++ src/environments/environment.mumbai.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   chainId: 80001,
   chainSymbol: 'MATIC',
-  contractAddress: '0xCF04b6864c4356b6924a13724922277dfd1313fc'
+  contractAddress: '0x1441D393DedF2aB832125fbDEB73E0033B498961'
 };
