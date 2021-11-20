762ef731d4b73244249870aceb2eea1a5e37ad61
Samuel VIDEAU <samuel.videau@efrei.net>
2021-11-20 23:45:28 +0100

    chore: new contract

--- src/environments/environment.kovan.ts
+++ src/environments/environment.kovan.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
   chainId: 42,
   chainSymbol: 'ETH',
-  contractAddress: '0x685511939fC3525aDec62fF1dcC331dD0B56139E'
+  contractAddress: '0x60c01323D8fBCFaf3d508C848BA20445d759dBc8'
 };
